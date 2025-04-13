cd02b956b5f0089008bab84fc5ccbcf7f14c7f28
codewithowais <codewithowais@gmail.com>
2025-04-13 17:18:37 +0500

    if else

--- main.dart
+++ main.dart
@@ -1,6 +1,26 @@
+import 'dart:io';
+
 void main() {
   int num1 = 10;
   int num2 = 10;
   print(num1);
   print(num2);
+  String? stdName = stdin.readLineSync();
+  int num19 = int.parse(stdin.readLineSync() ?? "0");
+  if (num1 != num2) {
+    print("running if block");
+    if (false) {
+      print("running else if block and if block");
+    } else {
+      print("running else if block and if block");
+    }
+  } else if (num1 == num2) {
+    print("running else if block");
+    if (true) {
+      print("running else if block and if block");
+    }
+  } else {
+    print("running else block");
+  }
+  print("if else done");
 }
